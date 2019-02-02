d54e71ca9146b5cac4ac9fb21e22ca09e8570383
Theofanis Despoudis <theo.despoudis@teckro.com>
2019-02-02 23:34:30 +0000

    Update go.sh

--- scripts/go.sh
+++ scripts/go.sh
@@ -2,9 +2,9 @@
 
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
-sudo apt-get install -y golang-1.10-go
+sudo apt-get install -y golang-1.11-go
 cat << ENDL > /etc/profile.d/exports.sh
-export GOROOT=/usr/lib/go-1.10
+export GOROOT=/usr/lib/go-1.11
 export GOPATH=~/go
 export PATH=$PATH:$GOROOT/bin
 ENDL
